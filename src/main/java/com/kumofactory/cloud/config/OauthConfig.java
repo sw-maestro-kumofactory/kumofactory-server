f90b00cf0e162f42589ad9f3c7861fce32b914ca
wook <gus5427@naver.com>
2023-06-10 17:08:04 +0900

    add OauthConfig 에 redirect url 추가

--- src/main/java/com/kumofactory/cloud/config/OauthConfig.java
+++ src/main/java/com/kumofactory/cloud/config/OauthConfig.java
@@ -1,12 +1,18 @@
 package com.kumofactory.cloud.config;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
+@Data
+@ConfigurationProperties(prefix = "oauth.github")
 public class OauthConfig {
-  @Value("${oauth.github.client.id}")
+  @Value("${oauth.github.client_id}")
   private String githubClientId;
-  @Value("${oauth.github.client.secret}")
+  @Value("${oauth.github.client_secret_id}")
   private String githubClientSecretId;
+  @Value("${oauth.github.get_access_token_url}")
+  private String getAccessTokenUrlFromGithub;
 }
