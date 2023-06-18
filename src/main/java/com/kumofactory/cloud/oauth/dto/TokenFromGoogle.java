c01509d2a6092925bc32d0903578985d2d927ecc
0-hae <abraxaschoi@gmail.com>
2023-06-18 22:34:36 +0900

    fix: include missing fields in TokenFromGoogle record (#8)
    TokenFromGoogle 레코드에서 빠진 다음과 같은 항목을 추가하였습니다.
    - expiresIn
    - idToken

--- src/main/java/com/kumofactory/cloud/oauth/dto/TokenFromGoogle.java
+++ src/main/java/com/kumofactory/cloud/oauth/dto/TokenFromGoogle.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record TokenFromGoogle(
         @JsonProperty("access_token") String accessToken,
+        @JsonProperty("expires_in") int expiresIn,
         @JsonProperty("scope") String scope,
-        @JsonProperty("token_type") String tokenType
+        @JsonProperty("token_type") String tokenType,
+        @JsonProperty("id_token") String idToken
 ) {}
