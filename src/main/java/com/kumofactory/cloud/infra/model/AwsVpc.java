1e0555459126a8b25d889a2e84796a6d2fc2f49d
Hyeonuook Kim <gus5427@naver.com>
2023-06-28 01:07:25 +0900

    feat: add AwsVpc entity

--- src/main/java/com/kumofactory/cloud/infra/model/AwsVpc.java
+++ src/main/java/com/kumofactory/cloud/infra/model/AwsVpc.java
@@ -1,2 +1,19 @@
-package com.kumofactory.cloud.infra.model;public class AWS_VPC {
+package com.kumofactory.cloud.infra.model;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class AwsVpc {
+    @Id
+    @Column(name = "aws_vpc", nullable = false)
+    private Long id;
+
+    private String name;
+    private String cidr;
+    private String memberId;
 }
