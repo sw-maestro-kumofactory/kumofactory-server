f36347ce98b74c2da4df2fb0455e4f726a8faae8
coding-convention <my.ap.nal.changchang@gmail.com>
2023-07-01 17:45:58 +0900

    feat: add create, getUserId method on JwtTokenProvider (#8) access token과 refresh token을 반환해주는 create method와 jwt에서 user id를 반환해주는 메서드를 생성했습니다.

---
+++ src/main/java/com/kumofactory/cloud/jwt/provider/JwtTokenProvider.java
@@ -0,0 +1,66 @@
+package com.kumofactory.cloud.jwt.provider;
+
+import com.kumofactory.cloud.jwt.dto.TokenDto;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String secret;
+    @Value("${jwt.token-validity-in-milliseconds}")
+    private long VALIDITY;
+
+    public TokenDto create(String id) {
+
+        Date now = new Date();
+
+        String accessToken = Jwts.builder()
+                .setSubject(id)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + VALIDITY))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .compact();
+
+        String refreshToken = Jwts.builder()
+                .setSubject(id)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + (VALIDITY * 365)))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .compact();
+
+        return TokenDto.builder()
+                .userEmail(id)
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+
+    public String getUserId(String token) {
+        return Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(secret.getBytes())).build().parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public String validateAccessToken() {
+
+        return null;
+    }
+
+    public String validateRefreshToken() {
+
+        return null;
+    }
+
+    public String refreshAccessToken() {
+
+        return null;
+    }
+
+
+}
