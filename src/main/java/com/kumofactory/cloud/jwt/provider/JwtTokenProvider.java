384dede29711ba3dce7871206f0abaa66cbf30a8
Hyeonuook Kim <gus5427@naver.com>
2023-07-13 01:08:59 +0900

    feat : add validateToken

--- src/main/java/com/kumofactory/cloud/jwt/provider/JwtTokenProvider.java
+++ src/main/java/com/kumofactory/cloud/jwt/provider/JwtTokenProvider.java
@@ -1,6 +1,8 @@
 package com.kumofactory.cloud.jwt.provider;
 
 import com.kumofactory.cloud.jwt.dto.TokenDto;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
@@ -13,54 +15,62 @@ import java.util.Date;
 @RequiredArgsConstructor
 public class JwtTokenProvider {
 
-    @Value("${jwt.secret}")
-    private String secret;
-    @Value("${jwt.token-validity-in-milliseconds}")
-    private long VALIDITY;
+		@Value("${jwt.secret}")
+		private String secret;
+		@Value("${jwt.token-validity-in-milliseconds}")
+		private long VALIDITY;
 
-    public TokenDto create(String id) {
+		public TokenDto create(String id) {
 
-        Date now = new Date();
+				Date now = new Date();
 
-        String accessToken = Jwts.builder()
-                .setSubject(id)
-                .setIssuedAt(now)
-                .setExpiration(new Date(now.getTime() + VALIDITY))
-                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
-                .compact();
+				String accessToken = Jwts.builder()
+																 .setSubject(id)
+																 .setIssuedAt(now)
+																 .setExpiration(new Date(now.getTime() + VALIDITY))
+																 .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+																 .compact();
 
-        String refreshToken = Jwts.builder()
-                .setSubject(id)
-                .setIssuedAt(now)
-                .setExpiration(new Date(now.getTime() + (VALIDITY * 365)))
-                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
-                .compact();
+				String refreshToken = Jwts.builder()
+																	.setSubject(id)
+																	.setIssuedAt(now)
+																	.setExpiration(new Date(now.getTime() + (VALIDITY * 365)))
+																	.signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+																	.compact();
 
-        return TokenDto.builder()
-                .userEmail(id)
-                .accessToken(accessToken)
-                .refreshToken(refreshToken)
-                .build();
-    }
+				return TokenDto.builder()
+											 .userEmail(id)
+											 .accessToken(accessToken)
+											 .refreshToken(refreshToken)
+											 .build();
+		}
 
-    public String getUserId(String token) {
-        return Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(secret.getBytes())).build().parseClaimsJws(token).getBody().getSubject();
-    }
+		public String getUserId(String token) {
+				return Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(secret.getBytes())).build().parseClaimsJws(token).getBody().getSubject();
+		}
 
-    public String validateAccessToken() {
+		public boolean validateAccessToken(String token) {
+				try {
+						Jws<Claims> claims = Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(secret.getBytes())).build().parseClaimsJws(token);
+						return !claims.getBody().getExpiration().before(new Date());
+				} catch (Exception e) {
+						return false;
+				}
+		}
 
-        return null;
-    }
+		public boolean validateRefreshToken(String token) {
+				try {
+						Jws<Claims> claims = Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(secret.getBytes())).build().parseClaimsJws(token);
+						return !claims.getBody().getExpiration().before(new Date());
+				} catch (Exception e) {
+						return false;
+				}
+		}
 
-    public String validateRefreshToken() {
+		public String refreshAccessToken() {
 
-        return null;
-    }
-
-    public String refreshAccessToken() {
-
-        return null;
-    }
+				return null;
+		}
 
 
 }
