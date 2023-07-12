bfe07f89418604a0fa721658b549024427111909
Hyeonuook Kim <gus5427@naver.com>
2023-07-13 01:06:25 +0900

    feat : add mapping domain to dto in Dto class
    - 데이터 베이스에서 가져온 Entity 를 Dto 로 맵핑하는 함수 추가 
    - Mapper Class 를 따로 분리하지 않고 Dto 내부에 생성함수 추가

--- src/main/java/com/kumofactory/cloud/blueprint/dto/aws/AwsBluePrintDto.java
+++ src/main/java/com/kumofactory/cloud/blueprint/dto/aws/AwsBluePrintDto.java
@@ -1,7 +1,10 @@
 package com.kumofactory.cloud.blueprint.dto.aws;
 
+import com.kumofactory.cloud.blueprint.domain.PointLink;
+import com.kumofactory.cloud.blueprint.domain.aws.AwsComponent;
 import com.kumofactory.cloud.blueprint.dto.PointLinkDto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import lombok.Data;
@@ -11,7 +14,23 @@ import lombok.Getter;
 @Data
 public class AwsBluePrintDto {
 
-    private String name;
-    private List<AwsComponentDto> components;
-    private List<PointLinkDto> links;
+		private String name;
+		private List<AwsComponentDto> components;
+		private List<PointLinkDto> links;
+
+		public static List<AwsComponentDto> awsComponentDtosMapper(List<AwsComponent> awsComponentDtos) {
+				List<AwsComponentDto> awsComponentDtoList = new ArrayList<>();
+				for (AwsComponent awsComponent : awsComponentDtos) {
+						awsComponentDtoList.add(AwsComponentDto.mapper(awsComponent));
+				}
+				return awsComponentDtoList;
+		}
+
+		public static List<PointLinkDto> pointLinkDtosMapper(List<PointLink> pointLinks) {
+				List<PointLinkDto> pointLinkDtoList = new ArrayList<>();
+				for (PointLink pointLink : pointLinks) {
+						pointLinkDtoList.add(PointLinkDto.mapper(pointLink));
+				}
+				return pointLinkDtoList;
+		}
 }
