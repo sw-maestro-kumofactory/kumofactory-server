ac476aea018ca1c9b8036704575059fdb473a9f9
Hyeonuook Kim <gus5427@naver.com>
2023-07-13 01:06:46 +0900

    feat : add mapping domain to dto in Dto class
    - 데이터 베이스에서 가져온 Entity 를 Dto 로 맵핑하는 함수 추가 
    - Mapper Class 를 따로 분리하지 않고 Dto 내부에 생성함수 추가

--- src/main/java/com/kumofactory/cloud/blueprint/dto/PointLinkDto.java
+++ src/main/java/com/kumofactory/cloud/blueprint/dto/PointLinkDto.java
@@ -1,16 +1,28 @@
 package com.kumofactory.cloud.blueprint.dto;
 
 import javax.validation.constraints.NotNull;
+
+import com.kumofactory.cloud.blueprint.domain.PointLink;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class PointLinkDto {
 
-    @NotNull
-    private String id;
-    // Component uuid 값
-    @NotNull
-    private String source;
-    @NotNull
-    private String target;
+		@NotNull
+		private String id;
+		// Component uuid 값
+		@NotNull
+		private String source;
+		@NotNull
+		private String target;
+
+		public static PointLinkDto mapper(PointLink pointLink) {
+				PointLinkDto pointLinkDto = new PointLinkDto();
+				pointLinkDto.setId(pointLink.getUi_id());
+				pointLinkDto.setSource(pointLink.getSource_id());
+				pointLinkDto.setTarget(pointLink.getDestination_id());
+				return pointLinkDto;
+		}
 }
