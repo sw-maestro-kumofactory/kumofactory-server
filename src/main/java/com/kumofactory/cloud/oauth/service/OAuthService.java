e4a5dbde39d35fc0f5052d6c9d6252edebb96563
Hyeonuook Kim <gus5427@naver.com>
2023-08-05 17:00:50 +0900

    refactor : refactor

--- src/main/java/com/kumofactory/cloud/oauth/service/OAuthService.java
+++ src/main/java/com/kumofactory/cloud/oauth/service/OAuthService.java
@@ -1,7 +1,7 @@
 package com.kumofactory.cloud.oauth.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.kumofactory.cloud.config.OAuthProvider;
+import com.kumofactory.cloud.global.config.OAuthProvider;
 import com.kumofactory.cloud.auth.jwt.dto.TokenDto;
 import com.kumofactory.cloud.auth.jwt.provider.JwtTokenProvider;
 import com.kumofactory.cloud.member.MemberRepository;
@@ -9,10 +9,12 @@ import com.kumofactory.cloud.member.domain.Member;
 import com.kumofactory.cloud.oauth.dto.UserInfoDto;
 import com.kumofactory.cloud.oauth.service.github.GitHubService;
 import com.kumofactory.cloud.oauth.service.google.GoogleService;
+
 import static com.kumofactory.cloud.oauth.dto.OAuthDto.*;
 
 import lombok.extern.slf4j.Slf4j;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
@@ -27,88 +29,93 @@ import java.util.Map;
 @Slf4j
 @RequiredArgsConstructor
 public class OAuthService {
-    private final GoogleService googleService;
-    private final GitHubService githubService;
+		private final GoogleService googleService;
+		private final GitHubService githubService;
 
-    private final JwtTokenProvider jwtTokenProvider;
-    private final MemberRepository memberRepository;
+		private final JwtTokenProvider jwtTokenProvider;
+		private final MemberRepository memberRepository;
 
-    private TokenDto token;
+		private TokenDto token;
 
-    public ResponseEntity<String> request(OAuthProvider provider, String code)
-            throws JsonProcessingException {
+		public ResponseEntity<String> request(OAuthProvider provider, String code)
+						throws JsonProcessingException {
 
-        HttpHeaders responseHeaders = new HttpHeaders();
+				HttpHeaders responseHeaders = new HttpHeaders();
 
-        switch (provider) {
-            case GOOGLE: {
-                token = getAccessTokenFromGoogle(code);
-                break;
-            }
-            case GITHUB: {
-                token = getAccessTokenFromGitHub(code);
-                break;
-            }
-            default: {
-                throw new IllegalArgumentException("Unknown type of social login.");
-            }
-        }
+				switch (provider) {
+						case GOOGLE: {
+								token = getAccessTokenFromGoogle(code);
+								break;
+						}
+						case GITHUB: {
+								token = getAccessTokenFromGitHub(code);
+								break;
+						}
+						default: {
+								throw new IllegalArgumentException("Unknown type of social login.");
+						}
+				}
 
-        responseHeaders.add(HttpHeaders.CONTENT_TYPE, "application/json");
+				responseHeaders.add(HttpHeaders.CONTENT_TYPE, "application/json");
 
-        ResponseCookie refreshTokenCookie = ResponseCookie
-                .from("refreshToken", token.getRefreshToken())
-                .httpOnly(true)
-                .path("/")
-                .maxAge(Duration.ofDays(365))
-                .build();
+				ResponseCookie refreshTokenCookie = ResponseCookie
+								.from("refreshToken", token.getRefreshToken())
+								.httpOnly(true)
+								.path("/")
+								.maxAge(Duration.ofDays(365))
+								.build();
 
-        responseHeaders.add(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
+				responseHeaders.add(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
 
-        Map<String, String> map = new HashMap<>();
-        map.put("accessToken", token.getAccessToken());
+				Map<String, String> map = new HashMap<>();
+				map.put("accessToken", token.getAccessToken());
 
-        return ResponseEntity.ok()
-                .headers(responseHeaders)
-                .body(new ObjectMapper().writeValueAsString(map));
-    }
+				return ResponseEntity.ok()
+														 .headers(responseHeaders)
+														 .body(new ObjectMapper().writeValueAsString(map));
+		}
 
-    // TODO : 중복된 코드 간소화 (getAccessTokenFromXXX)
-    private TokenDto getAccessTokenFromGoogle(String code)
-            throws JsonProcessingException {
+		public TokenDto generateTestToken() {
+				UserInfoDto userInfoDto = new UserInfoDto("1234", "github");
+				return jwtTokenProvider.create(userInfoDto.id());
+		}
 
-        GoogleToken accessTokenFromGoogle = googleService.requestAccessToken(code);
-        if (accessTokenFromGoogle != null) {
+		// TODO : 중복된 코드 간소화 (getAccessTokenFromXXX)
+		private TokenDto getAccessTokenFromGoogle(String code)
+						throws JsonProcessingException {
 
-            UserInfoDto userInfo = googleService.requestUserInfo(accessTokenFromGoogle.accessToken());
-            saveMember(userInfo);
+				GoogleToken accessTokenFromGoogle = googleService.requestAccessToken(code);
+				if (accessTokenFromGoogle != null) {
 
-            return jwtTokenProvider.create(userInfo.id());
-        }
-        return null;
-    }
+						UserInfoDto userInfo = googleService.requestUserInfo(accessTokenFromGoogle.accessToken());
+						saveMember(userInfo);
 
-    private TokenDto getAccessTokenFromGitHub(String code)
-            throws JsonProcessingException {
+						return jwtTokenProvider.create(userInfo.id());
+				}
+				return null;
+		}
 
-        GitHubToken accessTokenFromGithub = githubService.requestAccessToken(code);
-        if (accessTokenFromGithub != null) {
+		private TokenDto getAccessTokenFromGitHub(String code)
+						throws JsonProcessingException {
 
-            UserInfoDto userInfo = githubService.requestUserInfo(accessTokenFromGithub.accessToken());
-            saveMember(userInfo);
+				GitHubToken accessTokenFromGithub = githubService.requestAccessToken(code);
+				if (accessTokenFromGithub != null) {
+
+						UserInfoDto userInfo = githubService.requestUserInfo(accessTokenFromGithub.accessToken());
+						saveMember(userInfo);
 
 
-            return jwtTokenProvider.create(userInfo.id());
-        }
-        return null;
-    }
+						return jwtTokenProvider.create(userInfo.id());
+				}
+				return null;
+		}
 
-    // 처음 가입한 member 일때만 저장
-    private void saveMember(UserInfoDto userInfo) {
-        Member member = memberRepository.findMemberByOauthId(userInfo.id());
-        if (member == null) {
-            member = Member.createMember(userInfo);
-            memberRepository.save(member);
-        }
-    }
+		// 처음 가입한 member 일때만 저장
+		private void saveMember(UserInfoDto userInfo) {
+				Member member = memberRepository.findMemberByOauthId(userInfo.id());
+				if (member == null) {
+						member = Member.createMember(userInfo);
+						memberRepository.save(member);
+				}
+		}
 }
