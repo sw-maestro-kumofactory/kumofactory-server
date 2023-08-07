3af28504b963f18a87dd5864435d1e15b163557a
Hyeonuook Kim <gus5427@naver.com>
2023-08-07 12:13:43 +0900

    af

--- src/main/java/com/kumofactory/cloud/blueprint/domain/aws/AwsComponent.java
+++ src/main/java/com/kumofactory/cloud/blueprint/domain/aws/AwsComponent.java
@@ -27,49 +27,41 @@ import static javax.persistence.EnumType.STRING;
 @TypeDef(name = "json", typeClass = JsonType.class)
 public class AwsComponent {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@CreationTimestamp
+	private Date created_at;
+	@UpdateTimestamp
+	private Date updated_at;
+	
+	@Column(unique = true)
+	private String ui_id; // Client 에서 생성하는 uuid
 
-    @CreationTimestamp
-    private Date created_at;
-    @UpdateTimestamp
-    private Date updated_at;
+	@Enumerated(STRING)
+	private AwsComponentType type; // Component 타입 (vm, vpc, subnet, ...)
+	
+	@Type(type = "json")
+	@Column(columnDefinition = "json")
+	private Map<String, Object> options;
 
-    @Column(unique = true)
-    private String ui_id; // Client 에서 생성하는 uuid
+	// Component 의 좌측 상단 좌표
+	private Integer position_x;
+	private Integer position_y;
 
-    @Enumerated(STRING)
-    private AwsComponentType type; // Component 타입 (vm, vpc, subnet, ...)
+	@ManyToOne
+	private AwsBluePrint bluePrint;
 
-    @Enumerated(STRING)
-    private AccessScope scope;
-
-    @Enumerated(STRING)
-    private AvailabilityZone az;
-
-    @Type(type = "json")
-    @Column(columnDefinition = "json")
-    private Map<String, Object> options;
-
-    // Component 의 좌측 상단 좌표
-    private Integer position_x;
-    private Integer position_y;
-
-    @ManyToOne
-    private AwsBluePrint bluePrint;
-
-    // ============== 생성함수 ================= //
-    public static AwsComponent createAwsComponent(AwsComponentDto awsComponentDto, AwsBluePrint awsBluePrint) {
-        AwsComponent awsComponent = new AwsComponent();
-        awsComponent.setUi_id(awsComponentDto.getId());
-        awsComponent.setPosition_x(awsComponentDto.getX());
-        awsComponent.setPosition_y(awsComponentDto.getY());
-        awsComponent.setType(awsComponentDto.getType());
-        awsComponent.setAz(awsComponentDto.getAz());
-        awsComponent.setScope(awsComponentDto.getScope());
-        awsComponent.setOptions(awsComponentDto.getOptions());
-        awsComponent.setBluePrint(awsBluePrint);
-        return awsComponent;
-    }
+	// ============== 생성함수 ================= //
+	public static AwsComponent createAwsComponent(AwsComponentDto awsComponentDto, AwsBluePrint awsBluePrint) {
+			AwsComponent awsComponent = new AwsComponent();
+			awsComponent.setUi_id(awsComponentDto.getId());
+			awsComponent.setPosition_x(awsComponentDto.getX());
+			awsComponent.setPosition_y(awsComponentDto.getY());
+			awsComponent.setType(awsComponentDto.getType());
+			awsComponent.setOptions(awsComponentDto.getOptions());
+			awsComponent.setBluePrint(awsBluePrint);
+			return awsComponent;
+	}
 }
