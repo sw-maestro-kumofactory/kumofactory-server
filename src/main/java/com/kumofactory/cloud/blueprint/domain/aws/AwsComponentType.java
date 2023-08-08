b2e060690c6e0fc75000ce69cab82fa00297cd78
wook <gus5427@naver.com>
2023-08-08 14:34:58 +0900

    update : RDS -> RDS_MYSQL

--- src/main/java/com/kumofactory/cloud/blueprint/domain/aws/AwsComponentType.java
+++ src/main/java/com/kumofactory/cloud/blueprint/domain/aws/AwsComponentType.java
@@ -1,5 +1,5 @@
 package com.kumofactory.cloud.blueprint.domain.aws;
 
 public enum AwsComponentType {
-    VPC, SUBNET, ALB, NLB, EC2, EFS, WAF, ROUTE53, NAT_GATEWAY, RDS, S3, ELASTIC_CACHE, CLOUDFRONT, AUTO_SCALING
+    VPC, SUBNET, ALB, NLB, EC2, EFS, WAF, ROUTE53, NAT_GATEWAY, RDS_MYSQL, S3, ELASTIC_CACHE, CLOUDFRONT, AUTO_SCALING
 }
