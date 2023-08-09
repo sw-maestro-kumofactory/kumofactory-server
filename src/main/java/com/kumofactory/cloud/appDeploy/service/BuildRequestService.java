862a1ae24ab8936e93627bfa7f97cb01db5166fc
coding-convention <my.ap.nal.changchang@gmail.com>
2023-08-10 03:04:19 +0900

    feat: find member via oauthId #16

--- src/main/java/com/kumofactory/cloud/appDeploy/service/BuildRequestService.java
+++ src/main/java/com/kumofactory/cloud/appDeploy/service/BuildRequestService.java
@@ -1,66 +1,7 @@
 package com.kumofactory.cloud.appDeploy.service;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.kumofactory.cloud.appDeploy.dto.BuildRequestDto;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-import org.springframework.web.client.RestTemplate;
 
-@Service
-@Slf4j
-public class BuildRequestService {
-    private final Logger logger = LoggerFactory.getLogger(BuildRequestService.class);
-    private String token = null;
-    private String baseUri = "https://api.github.com";
-    @Value("${build.server}")
-    private String buildServerUri = "http://3.38.34.254:8080";
-
-    public void RequestBuild(BuildRequestDto request) {
-        String url = buildServerUri + "/api/v1/deploy";
-        request.setDockerfile(isDockerfileExist(request.user(), request.repo()));
-        request.setgithubToken(token);
-
-        logger.info(request.toString());
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Accept", "application/json");
-        HttpEntity<BuildRequestDto> httpEntity = new HttpEntity<>(request, headers);
-        ResponseEntity<String> response = new RestTemplate().exchange(url, HttpMethod.POST, httpEntity, String.class);
-
-        if (response.getStatusCode().is2xxSuccessful()) {
-            logger.info("response : {}", response.getBody());
-        } else {
-            logger.error("response : {}", response.getBody());
-            logger.error("response : {}", response.getStatusCode());
-        }
-    }
-
-    private Boolean isDockerfileExist(String userName ,String repoName) {
-        String url = baseUri + "/repos/" + userName + "/" + repoName + "/contents/Dockerfile";
-        try {
-            ResponseEntity<JsonNode> response = RequestGitHubAPIs(url);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    private ResponseEntity<JsonNode> RequestGitHubAPIs(String uri) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Accept", "application/json");
-        if( StringUtils.hasText(token) ) {
-            headers.setBearerAuth(token);
-        }
-        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
-        return new RestTemplate().exchange(uri, HttpMethod.GET, httpEntity, JsonNode.class);
-    }
+public interface BuildRequestService {
+    public void RequestBuild(BuildRequestDto request, String oauthId);
 }
