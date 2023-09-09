870e5b356538f3acba8b3bbca2def0a5139b3e5a
nookcoder <gus5427@naver.com>
2023-09-09 16:09:44 +0900

    feat : add aws-cdk-result message listener

--- src/main/java/com/kumofactory/cloud/global/rabbitmq/domain/AwsCdkRequestDto.java
+++ src/main/java/com/kumofactory/cloud/global/rabbitmq/domain/AwsCdkRequestDto.java
@@ -1,2 +1,23 @@
-package com.kumofactory.cloud.global.rabbitmq.domain;public class AwsCdkRequestDto {
+package com.kumofactory.cloud.global.rabbitmq.domain;
+
+import com.kumofactory.cloud.blueprint.dto.aws.AwsCdkDto;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+public class AwsCdkRequestDto {
+    private String pattern;
+    private List<AwsCdkDto> body;
+
+    public static AwsCdkRequestDto buildAwsCdkRequestDto(String pattern, List<AwsCdkDto> body) {
+        return AwsCdkRequestDto.builder()
+                .pattern(pattern)
+                .body(body)
+                .build();
+    }
 }
--- src/main/java/com/kumofactory/cloud/global/rabbitmq/domain/AwsCdkResponseDto.java
+++ src/main/java/com/kumofactory/cloud/global/rabbitmq/domain/AwsCdkResponseDto.java
@@ -1,2 +1,27 @@
-package com.kumofactory.cloud.global.rabbitmq.domain;public class AwsCdkResponseDto {
+package com.kumofactory.cloud.global.rabbitmq.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.kumofactory.cloud.blueprint.domain.ProvisionStatus;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class AwsCdkResponseDto {
+    private String pattern;
+    private Object data;
+    private String id;
+
+    public static AwsCdkResponseDto createResponse(String pattern, Object data, String id) {
+        return AwsCdkResponseDto.builder()
+                .pattern(pattern)
+                .data(data)
+                .id(id)
+                .build();
+    }
+
 }
+
