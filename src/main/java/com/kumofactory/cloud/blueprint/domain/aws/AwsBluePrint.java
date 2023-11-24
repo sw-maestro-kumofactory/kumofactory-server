5e083ddd7dd8e25375d02ee158a4a67c4ba37ff7
nookcoder <gus5427@naver.com>
2023-11-24 22:12:41 +0900

    ㄹㅁㄷㅈ

--- src/main/java/com/kumofactory/cloud/blueprint/domain/aws/AwsBluePrint.java
+++ src/main/java/com/kumofactory/cloud/blueprint/domain/aws/AwsBluePrint.java
@@ -6,10 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
